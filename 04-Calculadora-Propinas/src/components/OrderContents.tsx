6e98eaa2d1319e8fa69eed46fe07e5464a43d776
Adrián González <g.adrian.gonzalez.90@gmail.com>
2024-09-11 21:45:53 -0600

    Created new Project called: Calculator

---
+++ 04-Calculadora-Propinas/src/components/OrderContents.tsx
@@ -0,0 +1,25 @@
+import type { OrderItem } from "../types";
+
+type OrderContentsProps = {
+    order: OrderItem[]
+}
+
+export function OrderContents({order} : OrderContentsProps) {
+    return (
+        <div>
+            <h2 className="font-black text-4xl">Consumo</h2>
+
+            <div className="space-y-3 mt-5">
+                { order.length === 0 ?
+                    <p>La orden está vacía.</p> :
+                    (order.map(item => (
+                        <div key={item.id}>
+                            <p>{item.name}</p>
+                            <p>Precio: ${item.price}</p>
+                            <p>Cantidad: {item.quantity}</p>
+                        </div>
+                    ))) }
+            </div>
+        </div>
+    );
+}
